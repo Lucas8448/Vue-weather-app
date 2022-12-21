b951632fb20c570532ade6ab0979d8b1f41176ed
Lucas Bateson <lucas.bateson@gmail.com>
2022-12-21 20:52:32 +0100

    V1 weather app with Icons

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,8 +1,53 @@
-<script setup>
-</script>
-
 <template>
+  <div id="weather-app">
+    <form @submit.prevent="getWeather">
+      <label for="city">City:</label>
+      <input type="text" id="city" v-model="city">
+      <button type="submit">Get Weather</button>
+    </form>
+    <table v-if="weatherData" class="weather-data">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Temperature</th>
+          <th>Description</th>
+          <th>Icon</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="day in weatherData.list" class="day">
+          <td>{{ day.dt_txt }}</td>
+          <td>{{ day.main.temp }}</td>
+          <td>{{ day.weather[0].description }}</td>
+          <td>
+            <img :src="`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`" alt="Weather icon">
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<script>
+export default {
+  data() {
+    return {
+      city: "",
+      weatherData: null
+    }
+  },
+  methods: {
+    async getWeather() {
+      try {
+        const apiKey = "a905f5082bf7c0f9e7de51bbe714bd00";
+        const url = `https://api.openweathermap.org/data/2.5/forecast?q=${this.city}&units=metric&appid=${apiKey}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.weatherData = data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
+</script>
---
+++ weather/src/components/WeatherIcon.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <img :src="`https://openweathermap.org/img/wn/${icon}@2x.png`" alt="Weather icon">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WeatherIcon',
+    props: ['icon'],
+}
+</script>
