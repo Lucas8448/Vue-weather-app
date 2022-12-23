332137ea3fea1e02a87a37e95439a29a042b3648
Lucas Bateson <lucas.bateson@gmail.com>
2022-12-23 14:46:33 +0100

    Convert styling from tailwind to bootstrap

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,25 +1,28 @@
 <template>
-  <div id="weather-app">
-    <div class="bg-gray-900 py-4">
-      <div class="container mx-auto px-4 inline-flex items-center">
-        <form class="relative w-64 mx-auto" @submit.prevent="getWeather">
+  <div id="weather-app" class="container-fluid px-0">
+    <div class="bg-midnight-blue py-4">
+      <div class="container mx-auto px-4 d-flex justify-content-around align-items-center">
+        <form class="form-inline w-64 mx-auto" @submit.prevent="getWeather">
           <label for="city" class="sr-only">City:</label>
           <input type="text" id="city" v-model="city"
-            class="input bg-gray-800 rounded-full py-2 px-4 pl-10 text-md text-white focus:outline-none focus:shadow-outline">
+            class="form-control form-control-lg bg-dark rounded-full py-2 px-4 pl-10 text-md text-white focus:outline-none focus:shadow-outline">
           <label for="display-option" class="sr-only">Display Option:</label>
-          <select id="display-option" v-model="displayOption" class="select bg-gray-800 rounded-full py-2 px-4 pl-10 text-md text-white focus:outline-none focus:shadow-outline">
+          <select id="display-option" v-model="displayOption"
+            class="form-control form-control-lg bg-dark rounded-full py-2 px-4 pl-10 text-md text-white focus:outline-none focus:shadow-outline">
             <option value="hourly">Every 4h</option>
             <option value="daily">Daily</option>
           </select>
-          <button type="submit" class="bg-gray-600 rounded-full py-2 px-4 text-md text-white hover:bg-gray-700">Get
+          <br>
+          <button type="submit" class="btn btn-lg btn-light rounded-full py-2 px-4 text-md text-black hover:bg-gray-700">Get
             Weather</button>
         </form>
+        <br>
       </div>
     </div>
     <!-- Use the displayOption variable to determine which data to display -->
-    <table v-if="hourlyData[0] || dailyData[0]" class="weather-data w-full text-left table-collapse py-4 px-4">
-      <thead class="bg-gray-800">
-        <tr class="text-white">
+    <table v-if="hourlyData[0] || dailyData[0]" class="table table-dark weather-data w-full text-left table-collapse py-4 px-4">
+      <thead>
+        <tr>
           <th class="py-2 px-4">Time</th>
           <th class="py-2 px-4">Temperature</th>
           <th class="py-2 px-4">Description</th>
